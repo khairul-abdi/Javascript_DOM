e0363473350d5d5f2325a4181a060708aee9a332
khairul-abdi <kabdi384@gmail.com>
2020-02-01 17:18:20 +0700

    review lagi

--- DOM Events/script.js
+++ DOM Events/script.js
@@ -1,27 +1,28 @@
-// const p3 = document.querySelector('.p3');
+const p1 = document.querySelector('.p1');
 
-// function ubahWarnap2() {
-//     p2.style.backgroundColor = 'lightblue';
-// }
+function ubahWarnap2() {
+    p2.style.backgroundColor = 'lightblue';
+}
 
-// function ubahWarnap3() {
-//     p3.style.backgroundColor = 'lightgreen';
-// }
+function ubahWarnap1() {
+    p1.style.backgroundColor = 'lightgreen';
+}
 
-// const p2 = document.querySelector('.p2');
-// p2.onclick = ubahWarnap2;
+const p2 = document.querySelector('.p2');
+p2.onclick = ubahWarnap2;
 
 
-// const p4 = document.querySelector('section#b p');
-// p4.addEventListener('click', function () {
-//     const ul = document.querySelector('section#b ul');
-//     const liBaru = document.createElement('li');
-//     const teksLiBaru = document.createTextNode('item baru');
-//     liBaru.appendChild(teksLiBaru);
-//     ul.appendChild(liBaru);
+const p4 = document.querySelector('section#b p');
+p4.addEventListener('click', function () {
+    const ul = document.querySelector('section#b ul');
+    const liBaru = document.createElement('li');
+    const teksLiBaru = document.createTextNode('item baru');
+    liBaru.appendChild(teksLiBaru);
+    ul.appendChild(liBaru);
 
-// });
+});
 
+// Perbedaan dari Event Handler dan addEventListener
 
 const p3 = document.querySelector('.p3');
 // p3.onclick = function () {
